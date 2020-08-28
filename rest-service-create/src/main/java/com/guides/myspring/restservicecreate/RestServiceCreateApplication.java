89db091db57f9d063fb51fe94dd2f8e2631a81eb
bhanu <bhanu@LAPTOP-DQRV4G9K.lan>
2020-08-28 17:08:02 -0400

    Initial Commit

---
+++ rest-service-create/src/main/java/com/guides/myspring/restservicecreate/RestServiceCreateApplication.java
@@ -0,0 +1,13 @@
+package com.guides.myspring.restservicecreate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestServiceCreateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestServiceCreateApplication.class, args);
+	}
+
+}
---
+++ rest-service-create/src/main/java/com/guides/myspring/restservicecreate/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.guides.myspring.restservicecreate;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(RestServiceCreateApplication.class);
+	}
+
+}
